4519000583bd482a0c9948d2e66b3058c65bc10c
Jeff Timlin <jefftimlin@gmail.com>
2017-06-24 12:42:06 +0800

    initial

---
+++ BetterLoadSaveGame/AddOnMain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BetterLoadSaveGame
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class AddOnMain : MonoBehaviour
+    {
+        public void Start()
+        {
+            Debug.Log("***** " + SaveGameManager.GetSaveFiles(KSPUtil.ApplicationRootPath, HighLogic.SaveFolder));
+        }
+
+        public void Update()
+        {
+            //Debug.Log("Hello world? " + Time.realtimeSinceStartup);
+        }
+    }
+}
---
+++ BetterLoadSaveGame/SaveGameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BetterLoadSaveGame
+{
+    public static class SaveGameManager
+    {
+        public static IEnumerable<string> GetSaveFiles(string gameDir, string saveFolder)
+        {
+            return Directory.GetFiles(gameDir + "saves\\" + saveFolder, "*.sfs");
+        }
+    }
+}
