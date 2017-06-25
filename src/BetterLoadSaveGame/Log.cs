78cb0c9570a27cb191f5d9183cd636e2890f74f3
Jeff Timlin <jefftimlin@gmail.com>
2017-06-25 09:50:42 +0800

    Add logger

---
+++ src/BetterLoadSaveGame/Log.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BetterLoadSaveGame
+{
+    public static class Log
+    {
+        private static bool _enableLogging = true;
+
+        private static string GetLogMessage(object messageOrFormat, object[] args)
+        {
+            string message = messageOrFormat.ToString();
+            if (args != null && args.Length > 0)
+            {
+                message = String.Format(message, args);
+            }
+            return String.Format("[BLSG] {0}", message);
+        }
+
+        public static void Info(object messageOrFormat, params object[] args)
+        {
+            if (_enableLogging)
+            {
+                Debug.Log(GetLogMessage(messageOrFormat, args));
+            }
+        }
+
+        public static void Error(object messageOrFormat, params object[] args)
+        {
+            if (_enableLogging)
+            {
+                Debug.LogError(GetLogMessage(messageOrFormat, args));
+            }
+        }
+
+        public static void Warn(object messageOrFormat, params object[] args)
+        {
+            if (_enableLogging)
+            {
+                Debug.LogWarning(GetLogMessage(messageOrFormat, args));
+            }
+        }
+    }
+}
